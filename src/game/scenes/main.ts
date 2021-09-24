f547152c7497ba181a8486cea9e04fd6b81a58a8
Bill Early <earlybilly@gmail.com>
2021-09-23 23:35:44 -0400

    Collisions with other sprites

--- src/game/scenes/main.ts
+++ src/game/scenes/main.ts
@@ -10,7 +10,7 @@ export class MainScene extends Scene {
   private camera!: Cameras.Scene2D.Camera;
   private player!: Physics.Arcade.Sprite;
   private controls!: Controls;
-  private movementSpeed: number = 400;
+  private movementSpeed: number = 300;
 
   init() {
     this.controls = initializeControls(this);
@@ -22,11 +22,22 @@ export class MainScene extends Scene {
       'red-square',
       `${process.env.PUBLIC_URL}/sprites/red-square.png`
     );
+
+    this.load.image(
+      'blue-square',
+      `${process.env.PUBLIC_URL}/sprites/blue-square.png`
+    );
   }
 
   create() {
     this.player = this.physics.add.sprite(320, 240, 'red-square');
     this.player.setCollideWorldBounds(true);
+
+    const staticCollisionGroup = this.physics.add.staticGroup();
+    staticCollisionGroup.create(320, 280, 'blue-square').refreshBody();
+    staticCollisionGroup.create(420, 260, 'blue-square').refreshBody();
+
+    this.physics.add.collider(this.player, staticCollisionGroup);
   }
 
   update() {
